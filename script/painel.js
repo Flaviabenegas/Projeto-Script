12cc3c6d21f0d3b57202aacc435feb03740d36c2
flaviabenegas <flaviabenegas@gmail.com>
2026-03-13 14:05:27 -0300

    .

--- script/painel.js
+++ script/painel.js
@@ -1,11 +1,13 @@
+const loader = document.getElementById('loader')
 const login = document.getElementById('btn-login')
 const senha = document.getElementById('senha')
 const email = document.getElementById('email')
 
 login.addEventListener('click', (e) => {
     e.preventDefault()
-
-    if (email.value === "teste@teste.com" && senha.value === "teste") {
+    console.log("Email digitado:", email.value);
+    console.log("Senha digitada:", senha.value);
+    if (email.value.trim() === "teste@teste.com" && senha.value === "teste") {
         login.disabled = true;
         login.innerText = "Aguarde...";
         loader.classList.remove('d-none');
